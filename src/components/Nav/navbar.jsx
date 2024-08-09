9941f09ce6948fc445b8bba0a31fe729bdf93af8
nadiaputrirah <nadiapputriirah@gmail.com>
2024-08-09 17:27:40 +0700

    update page alur-belajar dengan react

--- src/components/Nav/navbar.jsx
+++ src/components/Nav/navbar.jsx
@@ -33,7 +33,7 @@ export default function Navigation() {
                         <a href="/" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Beranda</a>
                     </li>
                     <li>
-                        <a href="/project" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Alur Belajar</a>
+                        <a href="/alur-belajar" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Alur Belajar</a>
                     </li>
                     <li>
                         <a href="/skills" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-400 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Login</a>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/home.jsx";
+import Alur from "../pages/alur-belajar.jsx";
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/alur-belajar" element={<Alur />} />
             </Routes>
         </BrowserRouter>
     );
